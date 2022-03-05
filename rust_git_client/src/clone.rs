fd83d0601c21d092af0fdd864ead25a1d9b485ad
sushruth13 <hsushruth@gmail.com>
2022-03-04 21:43:43 -0500

    fixed issues with private repo auth

--- rust_git_client/src/clone.rs
+++ rust_git_client/src/clone.rs
@@ -3,15 +3,42 @@ use std::env;
 use std::path::Path;
 pub fn clone(){
 
-    let url = "https://github.com/sushruth13/Airbnb.git";
-    // let current_dir = std::env::current_dir();
-    // let current_dir=String::from(current_dir);
-    let repo = match Repository::clone(url, "test-tmp") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to clone: {}", e),
-    };
+    // let url = "https://github.com/sushruth13/Airbnb.git";
+    // // let current_dir = std::env::current_dir();
+    // // let current_dir=String::from(current_dir);
+    // let repo = match Repository::clone(url, "test-tmp") {
+    //     Ok(repo) => repo,
+    //     Err(e) => panic!("failed to clone: {}", e),
+    // };
    
-   
+    use git2::{Cred, Error, RemoteCallbacks};
+    use std::env;
+    use std::path::Path;
+    
+      // Prepare callbacks.
+      let mut callbacks = RemoteCallbacks::new();
+      callbacks.credentials(|_url, username_from_url, _allowed_types| {
+        Cred::ssh_key(
+          username_from_url.unwrap(),
+          None,
+          Path::new(&format!("{}/.ssh/id_ed25519", env::var("HOME").unwrap())),
+          None,
+        )
+      });
+    
+      // Prepare fetch options.
+      let mut fo = git2::FetchOptions::new();
+      fo.remote_callbacks(callbacks);
+    
+      // Prepare builder.
+      let mut builder = git2::build::RepoBuilder::new();
+      builder.fetch_options(fo);
+    
+      // Clone the project.
+      builder.clone(
+        "git@github.com:sushruth13/private_testRepo.git",
+        Path::new("test-tmp"),
+      ).unwrap();
 
 
   
