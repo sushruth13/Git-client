c882a3499fd7534ecc85a0499c04c3817a14905a
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2022-04-08 18:02:12 -0400

    comments added

--- rust_git_client/src/log.rs
+++ rust_git_client/src/log.rs
@@ -1,10 +1,12 @@
 use git2::{Commit, ObjectType, Repository};
 
+//Need to update timestamp in commit struct
 pub fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
     let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
     obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
+//lists all previous commits from git2 commit struct
 pub fn display_commit(commit: &Commit) {
     let timestamp = commit.time().seconds();
     let tm = time::at(time::Timespec::new(timestamp, 0));
