00828d633f4a62f2d94f833f0022f4d38a34eba5
RutujaTalekar <ritu1698talekar@gmail.com>
2022-04-08 18:12:50 -0400

    GitClient v2 final commit

--- rust_git_client/src/branch.rs
+++ rust_git_client/src/branch.rs
@@ -4,59 +4,21 @@ use std::option::Option as Option;
 use colored::*;
 
 struct BranchCommitTime {
-    branch_name: String,
-    last_commit: DateTime<Utc>,
-    hash: String
+    branch: String,
+    last_commit_time: DateTime<Utc>,
+    Key_hash: String,
 }
 
-pub fn show_branches_and_time(dir: &str) {
-    //let path="test-branch";
-
-    let repo = match Repository::open(dir) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to clone: {}", e),
+pub fn find_all_locals(repo: &Repository) -> git2::Branches {
+    let branches_list = match repo.branches(Option::Some(BranchType::Local)) {
+        Ok(br) => br,
+        Err(e) => panic!("failed to init: {}", e),
     };
 
-    // get all the local branches, and for each get the name and last commit time
-    // and return them as a new list of BranchCommitTime structs
-    let branches_list = get_all_local_branches(&repo);
-    let bct_list = branches_list.filter_map(|branch| {
-
-        // first get the name, or ignore the field when the name can't be determined
-        return match branch {
-            Ok((branch, _)) => {
-                let branch_name = get_branch_name(&branch);
-                let last_commit = get_branch_last_commit(&branch, &repo);
-
-                // flatmap these options to create an Option<BranchCommitTime>
-                let bct = branch_name
-                    .and_then(|n| last_commit.map(|t|
-                        BranchCommitTime { branch_name: n, last_commit: t.1, hash: t.0 }
-                    ));
-
-                    ;
-
-                bct
-            }
-            Err(_) => None
-        };
-    });
-
-    // collect the iterator in a vector so we can sort it. This has to
-    // be a mutable one, since we do sorting in place. Finally print out
-    // the sorted list to console.
-    let mut bct_v: Vec<_> = bct_list.collect();
-    bct_v.sort_by(|a, b| b.last_commit.cmp(&a.last_commit));
-    bct_v.iter().for_each(|bc| {
-
-        let hashed_name = hash_string(bc.branch_name.as_str());
-        println!("{}",  hashed_name.blue());
-        }
-    );
+    return branches_list;
 }
 
-/// replace all the characters in a string, except the first 3
-pub fn hash_string(name: &str) -> String {
+pub fn get_hash(name: &str) -> String {
     let mut hashed_name = String::from("");
     for (pos, e) in name.chars().enumerate() {
         match pos {
@@ -67,9 +29,7 @@ pub fn hash_string(name: &str) -> String {
     return hashed_name
 }
 
-/// We get the name, and convert it to a string, so we don't
-/// run into ownership issues, or need to pass the lifetime around.
-pub fn get_branch_name(branch: &Branch) -> Option<String> {
+pub fn find_name_of_branch(branch: &Branch) -> Option<String> {
     let name = match branch.name() {
         Ok(r) => r.map(|n| String::from(n)),
         Err(_) => None
@@ -78,7 +38,49 @@ pub fn get_branch_name(branch: &Branch) -> Option<String> {
     return name;
 }
 
-/// get the last commit time from a branch, if it fails return none
+pub fn display_time_branch(dir: &str) {
+
+    let repo = match Repository::open(dir) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to clone: {}", e),
+    };
+
+
+    let branches_list = find_all_locals(&repo);
+    let bct_list = branches_list.filter_map(|branch| {
+
+
+        return match branch {
+            Ok((branch, _)) => {
+                let branch_name = find_name_of_branch(&branch);
+                let last_commit = get_branch_last_commit(&branch, &repo);
+
+                
+                let bct = branch_name
+                    .and_then(|n| last_commit.map(|t|
+                        BranchCommitTime { branch: n, last_commit_time: t.1, Key_hash: t.0 }
+                    ));
+
+                    ;
+
+                bct
+            }
+            Err(_) => None
+        };
+    });
+
+
+    let mut bct_v: Vec<_> = bct_list.collect();
+    bct_v.sort_by(|a, b| b.last_commit_time.cmp(&a.last_commit_time));
+    bct_v.iter().for_each(|bc| {
+
+        let hashed_name = get_hash(bc.branch.as_str());
+        println!("{}",  hashed_name.blue());
+        }
+    );
+}
+
+
 pub fn get_branch_last_commit(branch: &Branch, repo: &Repository) -> Option<(String,DateTime<Utc>)> {
     let p = branch.get().target().and_then(|oid| {
         let commit = repo.find_commit(oid);
@@ -91,14 +93,4 @@ pub fn get_branch_last_commit(branch: &Branch, repo: &Repository) -> Option<(Str
         res
     });
     return p;
-}
-
-/// Get all the local branches for the passed in repository
-pub fn get_all_local_branches(repo: &Repository) -> git2::Branches {
-    let branches_list = match repo.branches(Option::Some(BranchType::Local)) {
-        Ok(br) => br,
-        Err(e) => panic!("failed to init: {}", e),
-    };
-
-    return branches_list;
 }
