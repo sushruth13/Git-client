aacff61df1aeba8b578a676ed32f1699b9a98d1f
sushruth13 <37882612+sushruth13@users.noreply.github.com>
2022-04-26 22:27:30 -0400

    Update git_commit.rs

--- rust_git_client/src/git_commit.rs
+++ rust_git_client/src/git_commit.rs
@@ -1,12 +1,13 @@
 use git2::{Commit, ObjectType, Repository,Oid, Signature};
 use std::path::Path;
 
-pub fn commit(path: &str, fpath:&str){
+pub fn commit(path: &str, fpath:&str, message:&str){
     let repo_root = path;
+    let message=message;
     let fpath= Path::new(fpath);
     let repo = Repository::open(repo_root).expect("Couldn't open repository");
     let commit = find_last_commit(&repo).expect("Couldn't find last commit");
-     let commit_id = add_and_commit(&repo,&fpath, "Added example file")
+     let commit_id = add_and_commit(&repo,&fpath, message)
     .expect("Couldn't add file to repo");
     println!("New commit: {}", commit_id);
     println!("{} state={:?}", repo.path().display(), repo.state());
