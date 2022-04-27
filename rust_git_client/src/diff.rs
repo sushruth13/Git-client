71c1a07002188abdcb409588b9e193bf8bd88185
RutujaTalekar <ritu1698talekar@gmail.com>
2022-04-26 21:41:21 -0400

    Revisions in git diff

--- rust_git_client/src/diff.rs
+++ rust_git_client/src/diff.rs
@@ -1,11 +1,11 @@
-#![allow(unused_variables, unused_imports)]
+#![allow(unused_variables, unused_imports, dead_code)]
 use git2::{Index, Error, DiffLineType, DiffOptions, Oid, Signature, Time, Repository, Tree};
 use std::borrow::Borrow;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
-pub fn diff_basic(path:  &str){
+pub fn diff_tree_to_workdir_fn(path:  &str){
 	//let mut old_tree = Tree::id(Oid);
 	//let path_root = Path::new("$HOME/Git-client/rust_git_client/.GitClient/");
 	//let path_root = Repository::workdir();
@@ -26,6 +26,43 @@ pub fn diff_basic(path:  &str){
 	//let mut index = repo.index().unwrap();
 	// index.add_path(path_root);
 	// let mut diff2 = repo.diff_tree_to_index(None, Some(&index), Some(&mut opts));
+	
+}
+
+
+pub fn diff_tree_to_index_fn(path:  &str){
+	let repo = Repository::open(path).expect("Couldn't open repository");
+	let path_root = Path::new(path);
+	let index = repo.index().unwrap();
+	//index.add_path(path_root);
+	let mut opts = DiffOptions::new();
+	opts.include_untracked(true);
+	//git diff --cached: It shows only those 
+	//changes of tracked files which are present in staging area.
+	let diff2 = repo.diff_tree_to_index(None, Some(&index), Some(&mut opts)).unwrap();
+	println!("lenght of deltas ---- {:}",diff2.deltas().len());
+	let stats1 = diff2.stats().unwrap();
+	println!("Number of insertions ----- {:}",stats1.insertions());
+	println!("Number of deletions ------ {:}",stats1.deletions());
+	println!("Total files changed ------ {:}",stats1.files_changed());
+	
+}
+
+pub fn diff_index_to_index_fn(path:  &str){
+	let repo = Repository::open(path).expect("Couldn't open repository");
+	let path_root = Path::new(path);
+	let index = repo.index().unwrap();
+	//index.add_path(path_root);
+	let mut opts = DiffOptions::new();
+	opts.include_untracked(true);
+	//git diff --cached: It shows only those 
+	//changes of tracked files which are present in staging area.
+	let diff2 = repo.diff_tree_to_index(None, Some(&index), Some(&mut opts)).unwrap();
+	println!("lenght of deltas ---- {:}",diff2.deltas().len());
+	let stats1 = diff2.stats().unwrap();
+	println!("Number of insertions ----- {:}",stats1.insertions());
+	println!("Number of deletions ------ {:}",stats1.deletions());
+	println!("Total files changed ------ {:}",stats1.files_changed());
 	
 }
 
